24098b75e42dc2facc8638bc469eeafcfbdec09c
deliasnchzgmz <78742129+deliasnchzgmz@users.noreply.github.com>
2022-08-24 20:10:34 +0200

    programa funcionando

--- tfg_tm/utils.py
+++ tfg_tm/utils.py
@@ -8,14 +8,13 @@ def image_preprocessing(image, threshold): # binariza la imagen y prepara para e
    
     return image_gray, bin_img
 
-
 def lines_location (bin_img, counter):
     line_pos = []
     delete_lines = []
-    ##comprueba las líneas que tienen negro
+    ##comprueba cuantos pixeles negros hay en cada fila
     for i in range(len(bin_img)):
         for j in range(len(bin_img[i])):
-            if bin_img[i][j]==0:
+            if bin_img[i][j] == 0:
                 counter[i] = counter[i] + 1
     #se queda con la posición de las líneas que tienen más de un X% de negro  
     for x in range(len(counter)):
@@ -27,9 +26,7 @@ def lines_location (bin_img, counter):
             delete_lines.append(duplicate)
     for delete in reversed(delete_lines):
         del line_pos[delete]
-
     return line_pos
-
 
 def staffs(line_pos):
   st = []
@@ -38,10 +35,12 @@ def staffs(line_pos):
     for group in range(0,num_staffs):
       diff = 0
       for space in range(1,len(line_pos)):
-        diff = diff + (line_pos[space]-line_pos[space-1]) ##media de los espacios para colocar una línea al mismo espacio por debajo del pentagrama
+        diff = diff + (line_pos[space]-line_pos[space-1]) 
+        #media de los espacios para colocar una línea al mismo espacio por debajo del pentagrama
 
       ##líneas del pentagrama
-      st.append([int(line_pos[0+5*group]-diff/4), line_pos[0+5*group], line_pos[1+5*group], line_pos[2+5*group], line_pos[3+5*group], line_pos[4+5*group]])
+      st.append([int(line_pos[0+5*group]-diff/4), line_pos[0+5*group], line_pos[1+5*group], 
+                    line_pos[2+5*group], line_pos[3+5*group], line_pos[4+5*group]])
 
       ##localización de los espacios del pentagrama
       st[group].insert(5,int((line_pos[3+5*group]+line_pos[4+5*group])/2))
@@ -49,27 +48,24 @@ def staffs(line_pos):
       st[group].insert(3,int((line_pos[1+5*group]+line_pos[2+5*group])/2))
       st[group].insert(2,int((line_pos[0+5*group]+line_pos[1+5*group])/2))
       st[group].insert(1,int((st[group][0]+line_pos[0+5*group])/2))
+  
   return st
 
-def detect_ei(bin_img, staff_lines):
-    firstLine = staff_lines[0][2]
-    lastLine = staff_lines[0][len(staff_lines[0])-1]
-    staff_width = lastLine - firstLine
+def detect_ei(bin_img):
+
     # Apply adaptiveThreshold at the bitwise_not of gray, notice the ~ symbol
     bin_img = cv2.bitwise_not(bin_img)
     bw = cv2.adaptiveThreshold(bin_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, \
                                cv2.THRESH_BINARY, 15, -2)
     h = np.copy(bw)
     horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
-    horizontalStructureD = cv2.getStructuringElement(cv2.MORPH_RECT, (80, 1))
+    horizontalStructureD = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 1))
     h = cv2.erode(h, horizontalStructure)
     h = cv2.dilate(h, horizontalStructureD)
-
 
     h = cv2.bitwise_not(h)
 
     return h
-
 
 def sortX(rect): ##ordenar las notas de derecha a izq
   if  (len(np.array(rect, object).shape)==2):
@@ -81,7 +77,6 @@ def find_nearest(array, value):
   difference_array = np.absolute(array-value)
   index = difference_array.argmin()
   return index
-
 
 def find_note(single_staff, rectY):
     notes = {
@@ -101,25 +96,20 @@ def find_note(single_staff, rectY):
 
     return rect_note   
 
-  
 def notes_per_staff(staff_lines, rect, h):
   notes_out = []
   out = []
-  inside_staff = []
-  number_staffs = len(staff_lines) ##cuántos pentagramas se han detectado?
+  number_staffs = len(staff_lines) #cuántos pentagramas se han detectado?
 
-
-  for p in range(0, number_staffs): ##para cada pentagrama
+  for p in range(0, number_staffs): #para cada pentagrama
     for r in range(0, len(rect)):
-      if rect[r][1]> staff_lines[p][0] and rect[r][1]< staff_lines[p][10]: ##si está dentro del pentagrama evaluado
         notes_out.append(rect[r])
     notes_out.sort(key=sortX)
-    for r in range(0, len(notes_out)): ##me quedo con la y de cada una y le sumo h/2 para hallar la nota
+    for r in range(0, len(notes_out)): #me quedo con la y de cada una y le sumo h/2 para hallar la nota
       notes_out[r] = notes_out[r][1]-h/2
       out.append(find_note(staff_lines[p], notes_out[r]))  
 
   return notes_out, out
-
 
 def get_rectangles_figures(mscore_img, template, staff_lines, threshold): ##Template matching y obtener rectángulos
     h = template.shape[0]
@@ -142,8 +132,7 @@ def get_rectangles_figures(mscore_img, template, staff_lines, threshold): ##Temp
     
     return rectangles_list, out
 
-
-def get_rectangles_clef(mscore_img, template,  name):
+def get_rectangles_gen(mscore_img, template,  name, threshold):
     h = template.shape[0]
     w = template.shape[1]
     result = cv2.matchTemplate(mscore_img, template, cv2.TM_CCOEFF_NORMED)
@@ -154,7 +143,6 @@ def get_rectangles_clef(mscore_img, template,  name):
        result = result+result_flipped
        
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    threshold = 0.85
     yloc, xloc = np.where(result>=threshold)
     
     out = []
@@ -170,21 +158,30 @@ def get_rectangles_clef(mscore_img, template,  name):
         rectangles_list = rectangles.tolist()
         out = [name]
     
-
-    
     return rectangles_list, out
-    
-    
+       
 def draw_rectangles(mscore_img, l):
-    
+    n = {
+      'C':'DO',
+      'D':'RE',
+      'E':'MI',
+      'F':'FA',
+      'G':'SOL',
+      'A':'LA',
+      'B':'SI',
+      'c':'DO*',
+      'd':'RE*',
+      'e':'MI*',
+      'f':'FA*'
+  }
+
     for i in range(len(l)):
         cv2.rectangle(mscore_img, (l[i][0][0], l[i][0][1]), (l[i][0][0]+l[i][0][2], l[i][0][1]+l[i][0][3]), (255, 0, 0), 2)
         if len(l[i])>2:
-            cv2.putText(mscore_img, l[i][2], (l[i][0][0], l[i][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            cv2.putText(mscore_img, n[l[i][2]], (l[i][0][0], l[i][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
         else: cv2.putText(mscore_img, l[i][1], (l[i][0][0], l[i][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-
-def order_lists(clef, clef_out, c, c_out, crotchet, crotchet_out, minim, minim_out, quaver, quaver_out, crotchetR, minimR, minimR_out, quaverR, quaverR_out):
+def order_lists(clef, clef_out, c, c_out, crotchet, crotchet_out, minim, minim_out, quaver, quaver_out, crotchetR, minimR, minimR_out, quaverR, quaverR_out, sharp_list, sharp_out, flat_list, flat_out, natural_list, natural_out):
     full_list = []
     full_list.append([clef[0], clef_out[0]])
     for i in range(len(c)):
@@ -203,10 +200,15 @@ def order_lists(clef, clef_out, c, c_out, crotchet, crotchet_out, minim, minim_o
         if minimR_out[i] == 'SI':
             full_list.append([minimR[i], 'z4'])
         else: full_list.append([minimR[i], 'z2'])
+    for i in range(len(sharp_list)):
+        full_list.append([sharp_list[i], sharp_out[0]])
+    for i in range(len(flat_list)):
+        full_list.append([flat_list[i], flat_out[0]])
+    for i in range(len(natural_list)):
+        full_list.append([natural_list[i], natural_out[0]])
     
     full_list.sort(key=sortX)
     return full_list
-
 
 def detect_quaver(crotchet, crotchet_out, quaver_stem, nolines_img):
     quaver_list = []
@@ -230,7 +232,8 @@ def detect_quaver(crotchet, crotchet_out, quaver_stem, nolines_img):
     if(len(loc)>0):
         for c in reversed(range(0,len(crotchet))):
             for l in range(len(loc)):
-                if(loc[l] and crotchet[c][0]==loc[l]):
+                if(loc[l] and (crotchet[c][0]==loc[l] or round(crotchet[c][0]+crotchet[c][2]*1.5)==loc[l] or 
+                    (abs(crotchet[c][0]-loc[c]<12) ) )):
                    delete.append(c)  
         for element in delete:
             if element not in ndelete:
@@ -238,10 +241,9 @@ def detect_quaver(crotchet, crotchet_out, quaver_stem, nolines_img):
         for d in (range(len(ndelete))):
             quaver_list.append(crotchet.pop(ndelete[d]))
             quaver_out.append(crotchet_out.pop(ndelete[d]))
-
+    
     return crotchet, crotchet_out, quaver_list[::-1], quaver_out[::-1]
-
-            
+           
 def concatenate(cropped_img):
     if len(cropped_img)<2: 
         return cropped_img[0]
